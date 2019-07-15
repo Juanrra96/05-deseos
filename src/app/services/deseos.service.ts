4f5a958b68b5e995571d311dfc89ff51d5b256da
Juanrra96 <juan96ramon@gmail.com>
2019-07-15 14:34:07 -0600

    commit inicial

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  listas: Lista[] = [];
+
+  constructor() { 
+
+    this.cargarStorage();
+  
+   // const lista1 = new Lista('Recolectar piedras del infinito');
+   // const lista2 = new Lista('Heroes a desaparecer');
+
+   // this.listas.push(lista1, lista2); 
+
+  }
+
+  crearLista( titulo: string ){
+    const nuevalista = new Lista(titulo);
+    this.listas.push(nuevalista);
+    this.guardarStorage();
+
+    return nuevalista.id;
+  }
+
+  obtenerLista( id: string | number ){
+    id = Number(id);
+
+    return this.listas.find( listaData => listaData.id === id );
+      
+      
+  }
+
+  guardarStorage(){
+
+    localStorage.setItem('data', JSON.stringify(this.listas) );
+
+  }
+
+  cargarStorage(){
+    
+    if ( localStorage.getItem('data') ){
+      this.listas = JSON.parse( localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
+  }
+
+
+
+}
+
